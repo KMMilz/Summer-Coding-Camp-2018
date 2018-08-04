62d1dce6aa7b8206f516f0bbe538b90296fc99be
KMMilz <32345316+KMMilz@users.noreply.github.com>
2018-08-04 12:40:51 -0400

    Added Construction Project

---
+++ Construction/src/construction/Construction.java
@@ -0,0 +1,26 @@
+package construction;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author dapcep
+ */
+public class Construction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("What's your name"); 
+       Scanner Scanthing = new Scanner(System.in);
+       String name = Scanthing.nextLine();
+       System.out.println("Kenneth" + name);   }
+       //system.out.println makes you give an imput into the console  
+    
+    
+    
+}
+   
+              
+  
