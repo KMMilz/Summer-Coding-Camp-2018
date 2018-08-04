6f31a748da912b9506247138af67bacd33629ea5
KMMilz <32345316+KMMilz@users.noreply.github.com>
2018-08-04 12:50:57 -0400

    added coding camp project

---
+++ CodingCamp2018/src/codingcamp2018/CodingCamp2018.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codingcamp2018;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dapcep
+ */
+public class CodingCamp2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String args[]){
+    System.out.println("Please enter a number: ");
+    Scanner input = new Scanner(System.in);
+    int Numbers;
+    Numbers = input.nextInt(); 
+    System.out.println("You have entered: " + Numbers);
+    }
+    
+}
